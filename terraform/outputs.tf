83a273ffc171ac299df67766d5d85b315079ef7d
James Cleveland <james.cleveland@engineerbetter.com>
2023-03-23 14:25:37 +0000

    use upperfcase for funs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
 
-output "azure_client_id" {
+output "AZURE_CLIENT_ID" {
   description = "GitHub secret for AZURE_CLIENT_ID"
   value       = azurerm_user_assigned_identity.this.client_id
 }
 
-output "azure_tenant_id" {
+output "AZURE_TENANT_ID" {
   description = "GitHub secret for AZURE_TENANT_ID"
   value       = azurerm_user_assigned_identity.this.tenant_id
 }
 
-output "azure_subscription_id" {
+output "AZURE_SUBSCRIPTION_ID" {
   description = "GitHub secret for AZURE_SUBSCRIPTION_ID"
   value       = data.azurerm_subscription.primary.subscription_id
 }
